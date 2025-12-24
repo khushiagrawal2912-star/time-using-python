81ec35224df79fe26ef98b142d63382e090a2ae7
khushi-learns-code <khushiagrawal2912@gmail.com>
2025-12-24 21:29:35 +0530

    Create time_using_library.py

---
+++ time_using_library.py
@@ -0,0 +1,9 @@
+import time
+timestamp =time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp =int(time.strftime('%H'))
+print(timestamp)
+timestamp =int(time.strftime('%M'))
+print(timestamp)
+timestamp =int(time.strftime('%S'))
+print(timestamp)
